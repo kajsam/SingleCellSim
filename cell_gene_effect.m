496aefb6beb81b218c3aa01ac9771a5ac114122d
Kajsa Møllersen <kajsa.mollersen@uit.no>
2018-11-02 14:22:41 -0400

    Noise by cell class

--- cell_gene_effect.m
+++ cell_gene_effect.m
@@ -1,4 +1,4 @@
-function p = cell_gene_effect(distr, param, block, p_const,m)
+function p = cell_gene_effect(distr, param, block, p_const,n,d,C, cell_effect, cell_genes)
 
 % Kajsa Mollersen (kajsa.mollersen@uit.no) October 9th 2018
 
@@ -9,18 +9,38 @@ function p = cell_gene_effect(distr, param, block, p_const,m)
 %           m - length of cell/gene effect vector
 
 if strcmp(distr,'Normal')
-  mu = param(1);
-  sigma = param(2);
+  sigma = param(2); 
+  p = zeros(n,d);
+  dg = cell_genes;
+  for b = 1: length(cell_effect)
+    rng('default')
+    mu = param(1)+cell_effect(b);
   
-  p_distr = random(distr,mu,sigma,ceil(m/block),1);
+    p_distr = random(distr,mu,sigma,ceil(length(dg)/block),1);
+  
+    p_distr(p_distr < p_const - 1) = p_const - 1;
+    p_distr(p_distr > 1 - p_const) = 1 - p_const;
+  
+  
+    for j = 1: length(dg)
+      p(C{b},dg(j)) = p_distr(ceil(j/block));
+    end
+  end
+  
+  rng('default')
+  mu = param(1);
+  dng = setdiff(1:d, dg);
+  whos dng
+  p_distr = random(distr,mu,sigma,ceil(length(dng)/block),1);
+  whos p_distr
   
   p_distr(p_distr < p_const - 1) = p_const - 1;
   p_distr(p_distr > 1 - p_const) = 1 - p_const;
   
-  p = zeros(m,1);
-  for i = 1: m
-    p(i) = p_distr(ceil(i/block));
+  for j = 1: length(dng)
+    p(:,dng(j)) = p_distr(ceil(j/block));
   end
+  
 end
 
 if strcmp(distr,'Beta')
